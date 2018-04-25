de0465d04125f16d02d93e2860a6e9d387e45aa5
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2018-04-25 14:02:40 +0200

    Visually more useful encoding.
    The Braille block is not ordered in a visually consistent way,
    but puts the two lowermost bits last and the six remaining in
    column order.
    
    The new encoding is simpler and in line with https://github.com/qntm/braille-encode.

--- Data/ByteString/Braille.hs
+++ Data/ByteString/Braille.hs
@@ -13,6 +13,9 @@ module Data.ByteString.Braille (bbits, toBbits, toBbits') where
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BSL
 import Data.Word
+import qualified Data.Vector.Unboxed as UArr
+
+import Data.List (sort)
 
 import Data.String (IsString(..))
 import qualified Text.Show.Pragmatic as PS
@@ -21,10 +24,10 @@ import Data.CallStack
 
 
 bbits :: (HasCallStack, IsString bs) => String -> bs
-bbits = fromString . map fromBrailleC
+bbits = fromString . map (toEnum . fromIntegral . fromBrailleC)
  where fromBrailleC c
         | c >= '⠀' && c <= '⣿'
-                     = toEnum $ fromEnum c - fromEnum '⠀'
+                     = UArr.unsafeIndex invPalette $ fromEnum c - fromEnum '⠀'
         | otherwise  = error
             $ "`bbits` can only take Braille characters, i.e '⠀'..'⡵'..'⣿'. Cannot handle '"
                   ++[c]++"'."
@@ -36,4 +39,28 @@ toBbits' :: BS.ByteString -> String
 toBbits' = map toBrailleC . BS.unpack
 
 toBrailleC :: Word8 -> Char
-toBrailleC = toEnum . (+fromEnum '⠀') . fromIntegral
+toBrailleC = UArr.unsafeIndex braillePalette . fromIntegral
+
+braillePalette :: UArr.Vector Char
+braillePalette = UArr.fromList $ "⠀⢀⠠⢠⠐⢐⠰⢰⠈⢈⠨⢨⠘⢘⠸⢸"
+                              ++ "⡀⣀⡠⣠⡐⣐⡰⣰⡈⣈⡨⣨⡘⣘⡸⣸"
+                              ++ "⠄⢄⠤⢤⠔⢔⠴⢴⠌⢌⠬⢬⠜⢜⠼⢼"
+                              ++ "⡄⣄⡤⣤⡔⣔⡴⣴⡌⣌⡬⣬⡜⣜⡼⣼"
+                              ++ "⠂⢂⠢⢢⠒⢒⠲⢲⠊⢊⠪⢪⠚⢚⠺⢺"
+                              ++ "⡂⣂⡢⣢⡒⣒⡲⣲⡊⣊⡪⣪⡚⣚⡺⣺"
+                              ++ "⠆⢆⠦⢦⠖⢖⠶⢶⠎⢎⠮⢮⠞⢞⠾⢾"
+                              ++ "⡆⣆⡦⣦⡖⣖⡶⣶⡎⣎⡮⣮⡞⣞⡾⣾"
+                              ++ "⠁⢁⠡⢡⠑⢑⠱⢱⠉⢉⠩⢩⠙⢙⠹⢹"
+                              ++ "⡁⣁⡡⣡⡑⣑⡱⣱⡉⣉⡩⣩⡙⣙⡹⣹"
+                              ++ "⠅⢅⠥⢥⠕⢕⠵⢵⠍⢍⠭⢭⠝⢝⠽⢽"
+                              ++ "⡅⣅⡥⣥⡕⣕⡵⣵⡍⣍⡭⣭⡝⣝⡽⣽"
+                              ++ "⠃⢃⠣⢣⠓⢓⠳⢳⠋⢋⠫⢫⠛⢛⠻⢻"
+                              ++ "⡃⣃⡣⣣⡓⣓⡳⣳⡋⣋⡫⣫⡛⣛⡻⣻"
+                              ++ "⠇⢇⠧⢧⠗⢗⠷⢷⠏⢏⠯⢯⠟⢟⠿⢿"
+                              ++ "⡇⣇⡧⣧⡗⣗⡷⣷⡏⣏⡯⣯⡟⣟⡿⣿"
+{-# NOINLINE braillePalette #-}
+
+invPalette :: UArr.Vector Word8
+invPalette
+   = UArr.fromList . map snd . sort $ zip (UArr.toList braillePalette) [0..]
+{-# NOINLINE invPalette #-}
